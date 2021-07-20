0656ebbb8db0fd7c25f3cfa0159dfe0dfd16acd2
Josh Halpern <jhhalpern@gmail.com>
2021-07-20 11:19:37 -0400

    added Show model

---
+++ server/src/models/Show.js
@@ -0,0 +1,20 @@
+const Model = require("./Model.js")
+
+class Show extends Model {
+  static get tableName() {
+    return "shows"
+  }
+
+  static get jsonSchema() {
+    return {
+      type: "object",
+      required: ["name"],
+      properties: {
+        name: {type: "string", minLength: 1, maxLength: 255},
+        description: {type: "string", minLength: 10}
+      }
+    }
+  }
+}
+
+module.exports = Show
