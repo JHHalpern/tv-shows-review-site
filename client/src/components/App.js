27abcf8609cee3e355b9198653e2c28ddee39e62
Josh Halpern <jhhalpern@gmail.com>
2021-07-21 10:19:39 -0400

    spacing adjustment

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,18 +1,14 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { hot } from "react-hot-loader/root";
-
 import getCurrentUser from "../services/getCurrentUser";
 import "../assets/scss/main.scss";
 import RegistrationForm from "./registration/RegistrationForm";
 import SignInForm from "./authentication/SignInForm";
 import TopBar from "./layout/TopBar";
 import ShowsList from "./ShowsList.js"
-<<<<<<< HEAD
 import NewShowForm from "./NewShowForm.js"
-=======
 import TVDetailsPage from "./TVDetailsPage";
->>>>>>> d8f512c29ec979dc189d653abe55d6023782c079
 
 const App = (props) => {
   const [currentUser, setCurrentUser] = useState(undefined);
